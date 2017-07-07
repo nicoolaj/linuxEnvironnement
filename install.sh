1caed0203427256ce846f6dc656633b7cc34a60c
Nicolas Jalibert <NJalibert@infotrafic.com>
2017-07-07 15:31:47 +0200

    correction du script d'installation il était impossible de copier les fichiers cachés

--- install.sh
+++ install.sh
@@ -1,28 +1,57 @@
 #!/usr/bin/env bash
 
-#### debut des fonctions ####
+#Déclaration des variables globales
+status_bash_dotglob=`get_bash_option dotglob`
+
+#### Instanciation des fonctions
 generic_install(){
 	mkdir -p /$FLD/
 	cp $FLD/* /$FLD/
 }
 
+
+get_bash_option(){
+	shopt |grep $1
+}
+
+set_bash_option(){
+	if [ "$2" = "off" ]; then
+		#pour off
+		OPT="-u"
+	else
+		#pour on
+		OPT="-s"
+	fi
+	shopt $OPT $1
+}
+
 installation_bin(){
-	FLD = usr/local/bin
-	installation_generic $FLD
+	FLD=usr/local/bin
+	generic_install $FLD
 	chmod +x /$FLD/*
 }
 
 installation_home(){
-	QUI = `whoami`
-	HOME_QUI = `getent passwd $QUI | cut -f6 -d:`
-	cp /home/user/* $HOME_QUI/
+	QUI=`whoami`
+	HOME_QUI=`getent passwd $QUI | cut -f6 -d:`
+	set_bash_option dotglob "on"
+	cp home/user/* $HOME_QUI/
+	set_bash_option "dotglob" "$status_bash_dotglob"
 }
 installation_debian(){
-	apt-get install -y ghostscript git-core bzip2 tar gzip unzip
-
+	apt-get install -y \
+	bzip2 \
+	ghostscript git-core gzip \
+	screen sshfs \
+	tar \
+	unzip \
 
 }
-#### Fin des fonctions ####
 
+
+#### Lancement des fonctions
 installation_bin
 installation_home
+
+#### suppression des variables globales
+unset status_bash_dotglob
